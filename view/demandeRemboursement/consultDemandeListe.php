c9faa1123e196c5cbb33b7757251d978c23d50c2
RCoppe <rafael.coppe.jr@gmail.com>
2021-11-22 14:11:07 +0100

    Correction d'un oubli sur l'affichage

--- view/demandeRemboursement/consultDemandeListe.php
+++ view/demandeRemboursement/consultDemandeListe.php
@@ -10,17 +10,17 @@ if (count($lesDemandes) == 0) {
         <thead class="table-light">
             <tr>
                 <th class="col">date de saisie</th>
-                <th scope="col">commentaire</th>
-                <th scope="col">montant</th>
                 <th scope="col">type de frais</th>
+                <th scope="col">montant</th>
+                <th scope="col">commentaire</th>
             </tr>
         </thead>
         <?php foreach ($lesDemandes as $uneDemande) {
             echo ("<tr>");
             echo ("<td>" . $uneDemande->getDateSaisie() . "</td>");
-            echo ("<td>" . $uneDemande->getCommentaire()  . "</td>");
+            echo ("<td>" . $uneDemande->getTypeFrais()->getLibelle()  . "</td>");
             echo ("<td>" . $uneDemande->getMontant() . "</td>");
-            echo ("<td>" . $uneDemande->getTypeFrais()->getLibelle() . "</td>");
+            echo ("<td>" . $uneDemande->getCommentaire() . "</td>");
             echo ("</tr>");
         } ?>
     </table>
