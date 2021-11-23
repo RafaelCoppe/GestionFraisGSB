35508b7095a484695f1ac162e6cff7d493c73376
RCoppe <rafael.coppe.jr@gmail.com>
2021-11-23 11:06:43 +0100

    Debut du travail sur le controller

---
+++ controller/DeplacementsPharmaciens.php
@@ -0,0 +1,54 @@
+<?php
+
+class DeplacementsPharmaciens extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        require_once(ROOT . '/model/repository/DeplacementPharmacieRepository.php');
+        require_once(ROOT . '/model/entity/DeplacementPharmacie.php');
+        require_once(ROOT . '/model/entity/Produits.php');
+    }
+    public function ajoutDemandeRemboursementForm()
+    {
+        $this->render("deplacementsPharmacies/ajoutDeplacement", array("title" => "Ajout d'un déplacement en pharmacie"));
+    }
+    public function ajoutDemandeRemboursementTrait()
+    {
+        session_start();
+        $idUtilConnecte = $_SESSION['id'];
+        $laDemande = new DemandeRemboursement(
+            null,
+            date('Y-m-d H:i:s'),
+            $_POST['montant'],
+            $_POST['commentaire'],
+            new TypeFrais($_POST['typeFrais'], null),
+            new Utilisateur($idUtilConnecte)
+        );
+        $uneDemandeRepository = new DemandeRemboursementRepository();
+        $ret = $uneDemandeRepository->ajoutDemandeRemboursement($laDemande);
+
+        //
+        if ($ret == false) {
+            $msg = "<p class='text-danger'>ERREUR : votre demande n'a pas été enregistrée</p>";
+        } else {
+            $_POST = array();
+            $msg = "<p class='text-success'>Votre demande a été enregistrée</p>";
+        }
+        //
+        $typeFraisRepository = new TypeFraisRepository();
+        $lesTypesFrais = $typeFraisRepository->getLesTypesFrais();
+        $this->render("demandeRemboursement/ajoutDemande", array("title" => "Ajout d'une demande de remboursement", "lesTypesFrais" => $lesTypesFrais, "msg" => $msg));
+    }
+
+    public function consultMesDemandeRemboursement()
+    {
+        session_start();
+        $idUtilConnecte = $_SESSION['profil'];
+        
+        $unDemRemboursRepository = new DemandeRemboursementRepository();
+        $lesDemandes = $unDemRemboursRepository->getMesDemandesRemboursement($idUtilConnecte);
+
+        $this->render("demandeRemboursement/consultDemandeListe", array("title" => "Liste des demandes de remboursement", "lesDemandes" => $lesDemandes));
+    }
+};
